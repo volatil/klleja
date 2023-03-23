c158b30afcc74458934fe3ce0e437359f957dbc1
Paulo Torrealba <paulo@buscalibre.com>
2023-03-23 15:42:26 -0300

    component header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
 function Header() {
 	return (
-		<nav>
-			<p>MENU !</p>
-		</nav>
+		<section style={{ background: "white", display: "flex", justifyContent: "space-between" }} id="menu">
+			<img style={{ width: "auto" }} src="/assets/images/logo.png" alt="KLLJA" />
+			<img style={{ width: "40px" }} src="/assets/svg/lupa.svg" alt="Buscar" />
+			<img style={{ width: "40px" }} src="/assets/svg/hamburguesa.svg" alt="MENU" />
+		</section>
 	);
 }
 export default Header;
