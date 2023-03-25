8051517f35ecd3a47d0c60e89ca5eb225f55f1e9
Paulo Torrealba <paulo@buscalibre.com>
2023-03-25 04:48:03 -0300

    css paginas internas

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,23 +4,25 @@ import css from "@/styles/Footer.module.css";
 const Footer = function () {
 	return (
 		<footer className={css.footer}>
-			<ul>
-				<li>
-					<Link href="/politicadeprivacidad">
-						<p>Política de privacidad</p>
-					</Link>
-				</li>
-				<li>
-					<Link href="/terminosycondiciones">
-						<p>Términos y condiciones</p>
-					</Link>
-				</li>
-				<li>
-					<Link href="/configuraciondecookies">
-						<p>Configuración de cookies</p>
-					</Link>
-				</li>
-			</ul>
+			<nav>
+				<ul>
+					<li>
+						<Link href="/politicadeprivacidad">
+							<p>Política de privacidad</p>
+						</Link>
+					</li>
+					<li>
+						<Link href="/terminosycondiciones">
+							<p>Términos y condiciones</p>
+						</Link>
+					</li>
+					<li>
+						<Link href="/configuraciondecookies">
+							<p>Configuración de cookies</p>
+						</Link>
+					</li>
+				</ul>
+			</nav>
 		</footer>
 	);
 };
