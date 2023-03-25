9cc947bcb1219a0304950a72e8915ef954655b7c
Paulo Torrealba <paulo@buscalibre.com>
2023-03-25 14:16:51 -0300

    set API

---
+++ src/helpers/CONST.js
@@ -0,0 +1,2 @@
+const API = "https://sheets.googleapis.com/v4/spreadsheets/1aZlC5KaMoyEPVqbMiH8_bPCsZoh65PBW9cm0HpG8Kjk/values/inventario?key=AIzaSyAOWV0qar-gsYcyp5yWS99GQlPVovkuxcU";
+export default API;
