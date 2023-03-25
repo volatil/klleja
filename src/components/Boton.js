748aa80a65ee140eb4b8b9c0d29f507617725a6f
Paulo Torrealba <paulo@buscalibre.com>
2023-03-25 14:16:45 -0300

    fix props

--- src/components/Boton.js
+++ src/components/Boton.js
@@ -2,10 +2,10 @@ import Link from "next/link";
 import estilo from "@/styles/Boton.module.css";
 
 function Boton(props) {
-	const { texto, ruta } = props;
+	const { texto, ruta, width } = props;
 
 	return (
-		<Link className={estilo.botonComponent} href={ruta}>
+		<Link style={{ width: `${width}` }} className={estilo.botonComponent} href={ruta || "/"}>
 			<p>{texto}</p>
 			<img src="/assets/svg/arrowright.svg" alt={texto} />
 		</Link>
