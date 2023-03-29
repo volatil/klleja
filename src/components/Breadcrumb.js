f61b9cda3ef2d1bcbf80e2c5d63690774ca161e8
Paulo Torrealba <paulo@buscalibre.com>
2023-03-29 00:43:33 -0300

    breadcrumb sin anchor's

--- src/components/Breadcrumb.js
+++ src/components/Breadcrumb.js
@@ -1,6 +1,8 @@
+import bread from "@/styles/Breadcrumb.module.css";
+
 function Breadcrumb(params) {
 	return (
-		<ul style={{ width: "850px", margin: "0 auto" }}>
+		<ul id={bread.breadcrumb}>
 			<li>tienda</li>
 			<li>categoria</li>
 			<li>producto</li>
