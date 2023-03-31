1a98160244a8250a9af815eb5a3a736ccc86885c
Paulo Torrealba <paulo@buscalibre.com>
2023-03-31 02:28:30 -0300

    Bloque suscripcion

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,47 +4,64 @@ import { LINKREDES } from "@/helpers/CONST";
 
 import css from "@/styles/Footer.module.css";
 
+const Suscripcion = function () {
+	return (
+		<section id={css.suscripcion}>
+			<div>
+				<p>Ingrese su correo electrónico para obtener un <strong>20% de descuento</strong> en su primer pedido, más las últimas noticias de Klleja.</p>
+				<div className={css.suscriptor}>
+					<input type="text" placeholder="Correo Electrónico" />
+					<button type="button">Inscrirse</button>
+				</div>
+			</div>
+		</section>
+	);
+};
+
 const Footer = function () {
 	return (
-		<footer className={css.footer}>
-			<div className={css.puntas} />
-			<nav>
-				<ul className={css.paginasinternas}>
-					<li>
-						<Link href="/politicadeprivacidad">
-							<p>Política de privacidad</p>
-						</Link>
-					</li>
-					<li>
-						<Link href="/terminosycondiciones">
-							<p>Términos y condiciones</p>
-						</Link>
-					</li>
-					<li>
-						<Link href="/configuraciondecookies">
-							<p>Configuración de cookies</p>
-						</Link>
-					</li>
-				</ul>
-				<ul className={css.redes}>
-					<li>
-						<a href={LINKREDES.facebook}>
-							<img src="/assets/svg/facebook.svg" alt="facebook" />
-						</a>
-					</li>
-					<li>
-						<a href={LINKREDES.twitter}>
-							<img src="/assets/svg/twitter.svg" alt="twitter" />
-						</a>
-					</li>
-					<li>
-						<a href={LINKREDES.tiktok}>
-							<img src="/assets/svg/tiktok.svg" alt="tiktok" />
-						</a>
-					</li>
-				</ul>
-			</nav>
-		</footer>
+		<>
+			<Suscripcion />
+			<footer className={css.footer}>
+				<div className={css.puntas} />
+				<nav>
+					<ul className={css.paginasinternas}>
+						<li>
+							<Link href="/politicadeprivacidad">
+								<p>Política de privacidad</p>
+							</Link>
+						</li>
+						<li>
+							<Link href="/terminosycondiciones">
+								<p>Términos y condiciones</p>
+							</Link>
+						</li>
+						<li>
+							<Link href="/configuraciondecookies">
+								<p>Configuración de cookies</p>
+							</Link>
+						</li>
+					</ul>
+					<ul className={css.redes}>
+						<li>
+							<a href={LINKREDES.facebook}>
+								<img src="/assets/svg/facebook.svg" alt="facebook" />
+							</a>
+						</li>
+						<li>
+							<a href={LINKREDES.twitter}>
+								<img src="/assets/svg/twitter.svg" alt="twitter" />
+							</a>
+						</li>
+						<li>
+							<a href={LINKREDES.tiktok}>
+								<img src="/assets/svg/tiktok.svg" alt="tiktok" />
+							</a>
+						</li>
+					</ul>
+				</nav>
+			</footer>
+		</>
 	);
 };
 export default Footer;
